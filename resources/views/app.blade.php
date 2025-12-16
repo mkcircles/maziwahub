3c565897cb2dd4ed590e738f86115ec6ef38dbc7
mkcircles <mkcircles@gmail.com>
2025-12-17 00:14:52 +0300

    feat: Add HTTPS option to Vite asset loading.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>MaziwaHub</title>
-        @vite(['resources/css/app.css','resources/js/app.js'])
-        <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js" defer></script>
-    </head>
-    <body class="bg-muted-50 text-muted-900 dark:bg-muted-950 dark:text-muted-100">
-        <div id="app"></div>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>MaziwaHub</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'], 'https')
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js" defer></script>
+</head>
+
+<body class="bg-muted-50 text-muted-900 dark:bg-muted-950 dark:text-muted-100">
+    <div id="app"></div>
+</body>
+
 </html>
