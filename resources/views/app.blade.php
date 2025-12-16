f4b33b554962cbda11d640811ab8b5602a270825
mkcircles <mkcircles@gmail.com>
2025-12-17 00:18:45 +0300

    refactor: Adjust HTTPS scheme enforcement for non-local environments and simplify Vite asset loading.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>MaziwaHub</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'], 'https')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js" defer></script>
 </head>
 
